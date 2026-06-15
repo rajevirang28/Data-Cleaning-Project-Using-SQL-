3b2aa4ac16d06ff6582543b9d6568ff62ad5af30
Virang Raje <rajevirang28@gmail.com>
2026-06-15 07:53:13 +0530

    SQL quries
    Added queries to handle NULL values in industry and total_laid_off columns, including updates and deletions.

--- p1.sql
+++ p1.sql
@@ -115,7 +115,52 @@ SET `date` = STR_TO_DATE(`date`, '%m/%d/%Y');
 ALTER TABLE layoffs_staging2
 MODIFY COLUMN `date` DATE;
 
+ SELECT *
+ FROM layoffs_staging2
+ WHERE total_laid_off IS NULL
+ AND percentage_laid_off IS NULL;
+ 
+ UPDATE layoffs_staging2
+ SET industry = NULL
+ WHERE industry = '';
 
+
+SELECT *
+FROM layoffs_staging2
+WHERE industry IS NULL
+OR industry = '';
+
+SELECT *
+FROM layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+WHERE (t1.industry IS NULL OR t1.industry = '')
+AND t2.industry IS NOT NULL;
+
+
+UPDATE layoffs_staging2 t1
+JOIN layoffs_staging2 t2
+	ON t1.company = t2.company
+SET t1.industry = t2.industry
+WHERE t1.industry IS NULL
+AND t2.industry IS NOT NULL;
+
+SELECT *
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL; 
+
+
+DELETE 
+FROM layoffs_staging2
+WHERE total_laid_off IS NULL
+AND percentage_laid_off IS NULL; 
+
+SELECT *
+FROM layoffs_staging2;
+
+ALTER TABLE layoffs_staging2
+DROP COLUMN row_num;
 
 
 
